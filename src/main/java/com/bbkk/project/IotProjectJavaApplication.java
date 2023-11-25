61b24a34108824333a3bb4fc23eaf126ae9a54ee
chenzx <qchenzexuan@vip.qq.com>
2023-11-25 15:04:30 +0800

    feat: init project

---
+++ src/main/java/com/bbkk/project/IotProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bbkk.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotProjectJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotProjectJavaApplication.class, args);
+    }
+
+}
