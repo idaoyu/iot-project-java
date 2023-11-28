10268d26ace26d07712031f5427d7b49ec6f2b9c
chenzx <qchenzexuan@vip.qq.com>
2023-11-28 21:50:45 +0800

    chore: 增加 web 模块配置文件

--- iotp-web/src/main/java/com/bbkk/project/IotpWebApplication.java
+++ iotp-web/src/main/java/com/bbkk/project/IotpWebApplication.java
@@ -3,11 +3,15 @@ package com.bbkk.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 一条秋刀鱼zz qchenzexuan@vip.qq.com
+ * @since 2023/11/28 21:23
+ **/
 @SpringBootApplication
 public class IotpWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IotpWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IotpWebApplication.class, args);
+    }
 
 }
---
+++ iotp-web/src/test/java/com/bbkk/project/EncryptSensitiveInfoTest.java
@@ -0,0 +1,27 @@
+package com.bbkk.project;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 加密敏感信息
+ *
+ * @author 一条秋刀鱼zz qchenzexuan@vip.qq.com
+ * @since 2023-11-28 21:15
+ **/
+@SpringBootTest
+public class EncryptSensitiveInfoTest {
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void getPass() {
+        String text = "";
+        System.out.println(stringEncryptor.encrypt(text));
+
+    }
+
+}
