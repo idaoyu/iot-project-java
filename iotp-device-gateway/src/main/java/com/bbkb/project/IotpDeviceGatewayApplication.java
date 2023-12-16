922100edd4013527185691124f77724ee98a25ee
chenzx <qchenzexuan@vip.qq.com>
2023-12-16 12:13:51 +0800

    feat: 设备网关 init

---
+++ iotp-device-gateway/src/main/java/com/bbkb/project/IotpDeviceGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.bbkb.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 一条秋刀鱼zz qchenzexuan@vip.qq.com
+ * @since 2023/12/16 12:10
+ **/
+@SpringBootApplication
+public class IotpDeviceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotpDeviceGatewayApplication.class, args);
+    }
+
+}
---
+++ iotp-device-gateway/src/test/java/com/bbkb/project/IotpDeviceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbkb.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotpDeviceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
