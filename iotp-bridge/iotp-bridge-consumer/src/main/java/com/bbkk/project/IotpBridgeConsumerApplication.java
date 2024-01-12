abca220ae887e6ce34780145e48664b725cb025a
chenzx <qchenzexuan@vip.qq.com>
2024-01-12 23:03:06 +0800

    refactor: 优化模块结构

---
+++ iotp-bridge/iotp-bridge-consumer/src/main/java/com/bbkk/project/IotpBridgeConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.bbkk.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 一条秋刀鱼zz qchenzexuan@vip.qq.com
+ * @since 2023/12/24 11:46
+ **/
+@SpringBootApplication
+public class IotpBridgeConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotpBridgeConsumerApplication.class, args);
+    }
+
+}
---
+++ iotp-dependency/iotp-dependency-openfeign/src/main/java/com/bbkk/project/config/OpenFeignConfig.java
@@ -0,0 +1,15 @@
+package com.bbkk.project.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenFeign 配置
+ *
+ * @author 一条秋刀鱼zz qchenzexuan@vip.qq.com
+ * @since 2024-01-12 21:48
+ **/
+@Configuration
+@EnableFeignClients
+public class OpenFeignConfig {
+}
